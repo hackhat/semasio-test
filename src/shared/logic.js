fee28c5bcc70b466fa6401797eb5937bc9c5628a
hackhat <hackhat.com@gmail.com>
2015-04-15 19:10:32 +0100

    Changes the function into a class.

--- src/shared/logic.js
+++ src/shared/logic.js
@@ -1,3 +1,103 @@
-module.exports = function(){
-    console.log('logic')
+var _ = require('lodash');
+
+
+
+
+
+/**
+ * Returns the greatest product of 2 numbers from the array that
+ * are multiples of 3.
+ *
+ *
+ * **How to use?**
+ * Make a new instance:
+ *
+ *     var logic = new Logic([1, 2, 3, 4, 5, 6]);
+ *
+ * And then calculate the value:
+ *
+ *     logic.calculate(); // Returns `30`;
+ *
+ * You can even change the initial numbers:
+ *
+ *     logic.setNumbers([1, 2, 3]);
+ *
+ * And calculate again:
+ *
+ *     logic.calculate(); // Returns `6`;
+ *
+ *
+ * **Why a class and not a nested function?**
+ * Using a class is better because we don't need to make complex, nested
+ * hard to test functions. Also the function is more efficient and uses less memory
+ * because doesn't create a new function each time you need to calculate a new value.
+ * @param {Number[]} numbers
+ */
+var Logic = function(numbers){
+    /**
+     * Contains the initial numbers.
+     * @private
+     * @type {Number[]}
+     */
+    this.__numbers = numbers;
+
+    /**
+     * Stores the valid outputs.
+     * @private
+     * @type {Number[]}
+     */
+    this.__validOutputs = [];
 }
+
+
+
+
+
+_.extend(Logic.prototype, {
+
+
+
+    /**
+     * Overrides the initial numbers.
+     * @param {Number[]} numbers
+     */
+    setNumbers: function(numbers){
+        this.__numbers = numbers;
+    },
+
+
+
+    /**
+     * Returns the greatest product of 2 numbers from the array that
+     * are multiples of 3.
+     * @param  {Number[]} numbers
+     * @return {Number}
+     */
+    calculate: function(numbers){
+        this.__permute(numbers);
+    },
+
+
+
+    /**
+     * Makes a single permutation.
+     * @private
+     * @param  {Number[]} numbers
+     * @return {Number}
+     */
+    __permute: function(numbers){
+        var l = numbers.length;
+        while(l--){
+
+        }
+    }
+
+
+
+})
+
+
+
+
+
+module.exports = Logic;
--- test/logic.js
+++ test/logic.js
@@ -1,6 +1,6 @@
 require    = require('../getWebpackRequire');
 var expect = require("chai").expect;
-var logic  = require('shared/logic')
+var Logic  = require('shared/Logic')
 
 
 
@@ -9,20 +9,33 @@ describe('logic', function(){
 
 
 
+    var logic;
+
+
+
+    beforeEach(function(){
+        logic = new Logic();
+    })
+
+
+
     it('Given an array of integers, find the maximum product between two numbers from the array, that is a multiple of 3.', function(){
-        expect(logic([1, 2, 3, 4, 5, 6])).to.be.equal(30);
+        logic.setNumbers([1, 2, 3, 4, 5, 6]);
+        expect(logic.calculate()).to.be.equal(30);
     })
 
 
 
     it('The result for the input {6,8,8,7,2,5} should be 48 = 6*8. Note that 8*8 is the greatest product (64) but 64 isn\'t divisible by 3.', function(){
-        expect(logic([6,8,8,7,2,5])).to.be.equal(48);
+        logic.setNumbers([6,8,8,7,2,5]);
+        expect(logic.calculate()).to.be.equal(48);
     })
 
 
 
     it('Given the input {1,9,2,4} the result should be  36 = 9*4, and not 81 (9*9) since you should\'t multiply a number by itself.', function(){
-        expect(logic([1,9,2,4])).to.be.equal(36);
+        logic.setNumbers([6,8,8,7,2,5]);
+        expect(logic.calculate()).to.be.equal(36);
     })
 
 
