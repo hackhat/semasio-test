8521d3859ab8fbda134c6dc126a85aafdcf2fd82
hackhat <hackhat.com@gmail.com>
2015-04-21 19:47:25 +0100

    Uncomment comments.

--- test/logic.js
+++ test/logic.js
@@ -80,7 +80,7 @@ describe('logic', function(){
 
 
 
-    it.only('Should be a O(n) algorithm.', function(){
+    it('Should be a O(n) algorithm.', function(){
         this.timeout(60000);
         var suite = new Benchmark.Suite;
         var generateNNumbers = function(n){
@@ -95,17 +95,25 @@ describe('logic', function(){
             logic.setNumbers(generateNNumbers(10));
             logic.calculate();
         })
+        .add('50 items', function(){
+            logic.setNumbers(generateNNumbers(50));
+            logic.calculate();
+        })
         .add('100 items', function(){
             logic.setNumbers(generateNNumbers(100));
             logic.calculate();
         })
         .on('complete', function(){
             var results = this;
+            // Uncomment the following lines to see the mean.
             // results.forEach(function(suite){
             //     console.log(suite.name, suite.stats.mean)
             // })
-            // console.log(results[1].stats.mean / results[0].stats.mean);
-            expect(results[1].stats.mean / results[0].stats.mean).to.be.within(7.5, 12.5);
+            // Uncomment the following lines to see the increase relative to the base.
+            console.log('x5', results[1].stats.mean / results[0].stats.mean);
+            console.log('x10', results[2].stats.mean / results[0].stats.mean);
+            expect(results[1].stats.mean / results[0].stats.mean).to.be.below(7);
+            expect(results[2].stats.mean / results[0].stats.mean).to.be.below(15);
         })
         .run({'async': false});
     })
